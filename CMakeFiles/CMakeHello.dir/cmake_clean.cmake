0e42e9f19309fcf955092cc361acb3813ac3c4b1
Baro_Nghi <pnbnghi66@gmail.com>
2021-08-23 17:33:37 +0700

    first test commit

---
+++ CMakeFiles/CMakeHello.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMakeHello.dir/main.c.o"
+  "CMakeFiles/CMakeHello.dir/main.c.o.d"
+  "CMakeFiles/CMakeHello.dir/sum.c.o"
+  "CMakeFiles/CMakeHello.dir/sum.c.o.d"
+  "CMakeHello"
+  "CMakeHello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CMakeHello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sum.h"
+
+int main(int argc, char** argv)
+{
+	printf("Hello, world\n");
+	printf("Sum of 3 and 5: %d",Sum(3,5));	
+
+	return 0;
+}
